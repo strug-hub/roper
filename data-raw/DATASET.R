abe84db557a9e0beb2d18ad53ec780cd15d6666b
hh-zhong <lehang.zhong@gmail.com>
2022-01-19 15:20:30 -0500

    edit readmd

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,6 +1,6 @@
 ## code to prepare `DATASET` dataset goes here
 
-count <- matrix(rnbinom(n=1e5, mu=100, size=1/0.5), ncol=100)
+count <- matrix(rnbinom(n = 1e5, mu = 100, size = 1 / 0.5), ncol = 100)
 
 
 
